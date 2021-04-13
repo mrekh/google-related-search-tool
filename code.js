e372234a3f7904e9e43b84289b8fa508e6c4fece
Alireza <mresmikhani.work@gmail.com>
2021-04-13 17:22:10 +0430

    looping over all input keywords & by default in main.xlsx, we have "Output" sheet (Do not need to create an output sheet in code)

--- code.js
+++ code.js
@@ -4,16 +4,19 @@ const convert = require("xml-js");
 
 // Target keywords
 let keywords = [];
+let queryResults = []; // Response body for each keyword
+let xlsxResult = []; // Value we want to write in our xlsx
 
 // Reading main.xlsx
 let workbook = xlsx.readFile("main.xlsx");
-let first_sheet_name = workbook.SheetNames[0]; // Get input sheet
-let worksheet = workbook.Sheets[first_sheet_name]; // Get worksheet
+let first_sheet_name = workbook.SheetNames[0]; // First sheet in workbook - Input
+let inputWorksheet = workbook.Sheets[first_sheet_name]; // Get Input worksheet
+let outputWorksheet = workbook.Sheets[workbook.SheetNames[1]]; // Get Output wordsheet
 
 // Loop over column A of input sheet
 for (let i = 2;;i++) {
   let address_of_cell = `A${i}`; // Get keywords from values of column A
-  let desired_cell = worksheet[address_of_cell]; // Find desired cell
+  let desired_cell = inputWorksheet[address_of_cell]; // Find desired cell
   let disired_value = (desired_cell ? desired_cell.v : undefined); // Get the value
   if (disired_value !== undefined) {
     keywords.push(disired_value);
@@ -35,28 +38,22 @@ function keywordCleaner(inputKeyword) {
   return str.slice(0, str.length - 1);
 }
 
-let URL = `https://google.com/complete/search?output=toolbar&hl=en&q=${keywordCleaner(keywords[2])}`;
-console.log(URL);
-let queryResults = [];
-let xlsxResult = [];
-
-// Name of sheet we want to create in our xlsx file
-let ws_name = "Output";
-
 // Creating a get request
 (async () => {
   try {
-    const response = await got(URL);
-    queryResults[0] = await convert.xml2js(response.body, {compact: true, spaces: 2});
+    for (let i = 0; i < keywords.length; i++) {
+      let URL = `https://google.com/complete/search?output=toolbar&hl=en&q=${keywordCleaner(keywords[i])}`;
+      const response = await got(URL);
+      queryResults[i] = await convert.xml2js(response.body, {compact: true, spaces: 2});
 
-    for (let num = 0; num < await queryResults[0].toplevel.CompleteSuggestion.length - 1; num++) {
-      xlsxResult.push([queryResults[0].toplevel.CompleteSuggestion[num].suggestion._attributes.data]);
+      for (let num = 0; num < await queryResults[i].toplevel.CompleteSuggestion.length - 1; num++) {
+        xlsxResult.push([queryResults[i].toplevel.CompleteSuggestion[num].suggestion._attributes.data]);
+      }
     }
 
     // Data of created sheet
     let ws_data = await xlsxResult;
-    let ws = xlsx.utils.aoa_to_sheet(ws_data); // Create a new sheet with our data
-    xlsx.utils.book_append_sheet(workbook, ws, ws_name); // Appending created sheet to xlsx file
+    xlsx.utils.sheet_add_aoa(outputWorksheet, ws_data); // Append data to a sheet
     xlsx.writeFile(workbook, "main.xlsx"); // Write data to xlsx file
 
     console.log("Let's enjoy! 🥳")
